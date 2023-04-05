99b744a2149a0fd2946d116d82dfea9e44dc9321
hoomili <hasadian@hotmail.com>
2023-04-05 15:03:09 -0700

    findKeyByValue submitted

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`⛔⛔⛔ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const findKeyByValue = function (object, value) {
+  for (let item in object) {
+    console.log(object[item]);
+    if (object[item] === value) {
+      return item;
+    }
+  }
+};
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
