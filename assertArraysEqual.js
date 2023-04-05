f612e4061616a7cdc6d6c2dd05f33708272ba29e
hoomili <hasadian@hotmail.com>
2023-04-04 20:05:10 -0700

    solved assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+const assertArraysEqual = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    console.log(`⛔⛔⛔ Assertion Failed: [${array1}] !== [${array2}]`);
+    return;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`⛔⛔⛔ Assertion Failed: [${array1}] !== [${array2}]`);
+      return
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
+  return
+};
+assertArraysEqual(['hello', 'world', 3], ['hello', 'world', 3]);
