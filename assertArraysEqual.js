79241c9f71456b022fd170d60b7e9f60990b85d2
hoomili <hasadian@hotmail.com>
2023-04-04 20:45:56 -0700

    updated the functions in assertArraysEqual.js and solved without.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,22 @@
-const assertArraysEqual = function(array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    console.log(`⛔⛔⛔ Assertion Failed: [${array1}] !== [${array2}]`);
-    return;
+    return false;
   }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      console.log(`⛔⛔⛔ Assertion Failed: [${array1}] !== [${array2}]`);
-      return
+      return false;
     }
+  }
+  return true; 
+};
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === false) {
+    console.log(`⛔⛔⛔ Assertion Failed: [${array1}] !== [${array2}]`);
+    return;
   }
   console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
   return
 };
 assertArraysEqual(['hello', 'world', 3], ['hello', 'world', 3]);
+assertArraysEqual(['hello', 'world', 3], ['hello', 'world', 4]);
+assertArraysEqual([1], []);
