dcbc91392a8759b8d1126168f25b721fcdf3347b
hoomili <hasadian@hotmail.com>
2023-04-05 13:19:25 -0700

    updated assertArraysEqual function if statement and finished countOnly.js

--- middle.js
+++ middle.js
@@ -10,7 +10,7 @@ const eqArrays = function(array1, array2) {
   return true; 
 };
 const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2) === false) {
+  if (!eqArrays(array1, array2)) {
     console.log(`⛔⛔⛔ Assertion Failed: [${array1}] !== [${array2}]`);
     return;
   }
