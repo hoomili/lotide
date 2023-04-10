8bbc535f08633fbf82ce82da9a95bd57d79d6d5d
hoomili <hasadian@hotmail.com>
2023-04-10 14:49:55 -0700

    refactor the lotide files to refrence the other files

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,8 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  if (actual !== expected) {
+    console.log(`⛔⛔⛔ Assertion Failed: [${actual}] !== [${expected}]`);
     return
   }
-  console.log(`⛔⛔⛔ Assertion Failed: [${actual}] !== [${expected}]`);
+  console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
 };
-assertEqual("lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 3);
-assertEqual("Hello", "Hello");
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,15 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`⛔⛔⛔ Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
 const head = function(arr) {
   let firstElement = arr[0];
   return firstElement;
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
+module.exports = head;
+
